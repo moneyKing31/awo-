4bc8cb95c5977c09ffebec86f42de0a433825ded
heimawl <1785969728@qq.com>
2022-06-30 19:45:36 +0800

    完成加加减减

--- src/App.vue
+++ src/App.vue
@@ -1,74 +1,30 @@
 <template>
-    <div id="app">
-        <table class="tb">
-            <tr>
-                <th>编号</th>
-                <th>品牌名称</th>
-                <th>创立时间</th>
-                <th>操作</th>
-            </tr>
-            <!-- 循环渲染的元素tr -->
-            <tr v-for="item in list" :key="item.id">
-                <td>{{ item.id }}</td>
-                <td>{{ item.name }}</td>
-                <td>{{ item.time }}</td>
-                <td>
-                    <button @click="btnFn(item.id)">删除</button>
-                </td>
-            </tr>
-            <tr v-if="list.length === 0">
-                <td colspan="4">没有数据咯~</td>
-            </tr>
-        </table>
-    </div>
+  <div id="app">
+    <ul>
+      <li v-for="(item, ind) in arr" :key="item">
+        <span>{{ item }}</span>
+        <button @click="del(ind)">删除</button>
+      </li>
+    </ul>
+    <button @click="add">生成</button>
+  </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            list: [
-                { id: 1, name: "奔驰", time: "2020-08-01" },
-                { id: 2, name: "宝马", time: "2020-08-02" },
-                { id: 3, name: "奥迪", time: "2020-08-03" },
-            ],
-        };
+  data() {
+    return {
+      arr: [1, 5, 3],
+    };
+  },
+  methods: {
+    add() {
+      this.arr.push(Math.floor(Math.random() * 99));
+      console.log(Math.random());
     },
-    methods:{
-        btnFn(id){
-            const index=this.list.findIndex((ele)=>ele.id==id);
-            this.list.splice(index,1);
-        }
-    }
+    del(index) {
+      this.arr.splice(index, 1);
+    },
+  },
 };
 </script>
-
-<style>
-#app {
-    width: 600px;
-    margin: 10px auto;
-}
-
-.tb {
-    border-collapse: collapse;
-    width: 100%;
-}
-
-.tb th {
-    background-color: #0094ff;
-    color: white;
-}
-
-.tb td,
-.tb th {
-    padding: 5px;
-    border: 1px solid black;
-    text-align: center;
-}
-
-.add {
-    padding: 5px;
-    border: 1px solid black;
-    margin-bottom: 10px;
-}
-</style>
