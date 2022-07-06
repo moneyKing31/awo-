74b7a5e0be8d928c2fd1bb78889f0a53902e5b53
heimawl <1785969728@qq.com>
2022-07-06 21:02:42 +0800

    完成表单

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,30 @@
 <template>
   <div>
-    <button @click="comName = 'UserName'" style="margin: 10px;">手机号注册</button>
-    <button @click="comName = 'UserInfo'" style="margin-bottom: 10px;">邮箱注册</button>
-
-    <div style="border: 1px solid blue;">
-      <keep-alive>
-        <component :is="comName"></component>
-      </keep-alive>
-    </div>
+    <UseTable1>
+      <template v-slot:age="scppe">
+        <input type="text" v-model="scppe.row.age">
+      </template>
+      <template v-slot:name="scppe">
+        <input type="text" v-model="scppe.row.name">
+      </template>
+      <template v-slot:2="scppe">
+        <p>{{ scppe.row.headImgUrl }}</p>
+      </template>
+      <template v-slot:1="scppe">
+        <a :href="scppe.row.headImgUrl">{{ scppe.row.headImgUrl }}</a>
+      </template>
+      <template v-slot:0="scppe">
+        <img style="width: 100px;" :src="scppe.row.headImgUrl">
+      </template>
+    </UseTable1>
   </div>
 </template>
 
 <script>
-import UserName from './components/01/UserInfo.vue'
-import UserInfo from './components/01/UserName.vue'
+import UseTable1 from './components/UseTogenerate.vue'
 export default {
-  data() {
-    return {
-      value: '',
-      comName: "UserName"
-    };
-  },
   components: {
-    UserName,
-    UserInfo
+    UseTable1
   }
 }
 </script>
---
+++ src/注册切换.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <button @click="comName = 'UserName'" style="margin: 10px;">手机号注册</button>
+    <button @click="comName = 'UserInfo'" style="margin-bottom: 10px;">邮箱注册</button>
+
+    <div style="border: 1px solid blue;">
+      <keep-alive>
+        <component :is="comName"></component>
+      </keep-alive>
+    </div>
+  </div>
+</template>
+
+<script>
+import UserName from './components/01/UserInfo.vue'
+import UserInfo from './components/01/UserName.vue'
+export default {
+  data() {
+    return {
+      value: '',
+      comName: "UserName"
+    };
+  },
+  components: {
+    UserName,
+    UserInfo
+  }
+}
+</script>
+
+<style>
+</style>
